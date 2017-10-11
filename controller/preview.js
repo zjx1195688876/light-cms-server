33de592b57e1118658084185dac27c02b7286423
hzzhangjinxin <hzzhangjinxin@corp.netease.com>
2017-10-11 22:50:59 +0800

    misc: fix preview controller about promise bug

--- controller/preview.js
+++ controller/preview.js
@@ -1,33 +1,35 @@
 const Preview = require('../models/preview.js');
 
+const previewBase = async (ctx, client) => {
+    const { pageId } = ctx.params;
+    // 返回给前台的结果中不包含数据库特有的_id和__v
+    let condition = {'id': pageId};
+    let opts = {
+        '_id': 0,
+        '__v': 0
+    };
+    let res;
+    try {
+        res = await Preview.find(condition, opts);
+    } catch (e) {
+        ctx.body = '页面无法预览，请稍后重试';
+        return;
+    }
+
+    const title = res.title || '';
+    const content = res.content || '';
+
+    await ctx.render(client, {
+        title,
+        content
+    });
+};
+
 module.exports = {
     async previewH5 (ctx) {
-        // console.log(ctx.params);
-        // const title = ctx.query.title || '';
-        // const content = 'previewH5 content';
-        // await ctx.render('H5', {
-        //     title,
-        //     content
-        // });
-
-        const { pageId } = ctx.params;
-        console.log(pageId);
-        // 返回给前台的结果中不包含数据库特有的_id和__v
-        let condition = {'id': pageId};
-        let opts = {
-            '_id': 0,
-            '__v': 0
-        };
-        await Preview.find(condition, opts)
-        .then(res => {
-            const title = res.title || '';
-            const content = res.content || '';
-            ctx.render('H5', {
-                title,
-                content
-            });
-        }, () => {
-            ctx.body = '页面无法预览，请稍后重试';
-        });
+        await previewBase(ctx, 'H5');
+    },
+    async previewPC (ctx) {
+        await previewBase(ctx, 'PC');
     }
 };
--- routers/preview.js
+++ routers/preview.js
@@ -2,39 +2,8 @@ const router = require('koa-router')();
 const Preview = require('../controller/preview');
 
 const routers = router
-    .get('/H5/:pageId', Preview.previewH5)
-    /*.get('/PC/:pageId', async (ctx) => {
-        console.log(ctx);
-        const title = ctx.query.title || '';
-        const content = 'previewPC content';
-        await ctx.render('PC', {
-            title,
-            content
-        });
-    })
-    .get('/H5/:pageId', async (ctx) => {
-        console.log(ctx.params);
-        Preview.find(condition, opts)
-        .then(res => {
-            console.log(res);
-            ctx.body = {
-                code: 200,
-                success: true,
-                message: '获取PC预览页面成功',
-                body: res
-            };
-        }, err => {
-            if (err) {
-                result.message = err;
-            }
-            ctx.body = result;
-        });
-        const title = ctx.query.title || '';
-        const content = 'previewH5 content';
-        await ctx.render('H5', {
-            title,
-            content
-        });
-    })*/;
+    .get('/h5/:pageId', Preview.previewH5)
+    .get('/pc/:pageId', Preview.previewPC)
+    ;
 
 module.exports = routers;
